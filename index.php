44a0c2dac5d8a63bb8800889c452a2b19088ed20
crod1234 <crod1234@gmail.com>
2021-08-18 13:43:10 -0400

    fix home page layout issue media in blog section media and font size

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 				<h1 class="section-title">Hire a Bagpiper</h1>
 					<a class="landing-page-button-a"  class="button" href="<?php echo "/hire-a-bagpiper";?>">
 						<button class="landing-page-button">
-							<h2 class="section-subtitle">Click here to learn more about hiring a bagpiper.</h2>
+							<h2 class="section-subtitle">Click to learn more about hiring a bagpiper.</h2>
 						</button>
 					</a>  
 				<p class="section-p">Our bagpipers are the best in the area. They will add magic to your special event.</p>
@@ -36,10 +36,10 @@
 				<h1 class="section-title">Learn to Play Bagpipes</h1>
 				<a class="landing-page-button-a"  class="button" href="<?php echo "/learn-to-play-the-bagpipes";?>">
 					<button class="landing-page-button">
-						<h2 class="section-subtitle">Click here to learn more about how to learn the bagpipes.</h2>
+						<h2 class="section-subtitle">Click here to learn the bagpipes.</h2>
 					</button>
 				</a>
-				<p class="section-p">Want to learn how to play the bagpipes? Our instructors will get you on your way to learning to play the bagpipes.</p>
+				<p class="section-p">Want to learn to play the bagpipes? Our instructors will teach you to play the bagpipes!</p>
 			</div>
 			<a class="pic-anchor"  class="button" href="<?php echo "/learn-to-play-the-bagpipes";?>">
 				<img src="./img/children-playing-bagpipes.jpg" alt="Button to Learn How to Play Bagpipes">
@@ -54,14 +54,14 @@
 				<h2 class="section-title">NYC Bagpipes FAQ's</h2>
 				<a class="landing-page-button-a"  class="button" href="<?php echo "/bagpipe-faq";?>">
 					<button class="landing-page-button">
-						<h3 class="section-subtitle">Click here to learn more about frequently asked questions about bagpiping.</h3>
+						<h3 class="section-subtitle">Click here to read frequently asked questions about bagpiping.</h3>
 					</button>
 				</a>
 				<p class="section-p">Click here to read our bagpipe FAQ.</p>
 			</div>
 		</section>
 		<div class="section-border"></div>
-		<section class="section section-even bottom-section">
+		<section class="section section-even bottom-home">
 			<div class="section-div">
 				<h2 class="section-title">NYC Bagpipes Blog</h2>
 				<a class="landing-page-button-a"  class="button" href="<?php echo "/bagpipe-blog";?>">
