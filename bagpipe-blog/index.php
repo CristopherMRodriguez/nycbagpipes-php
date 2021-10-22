4ab3c8af7ad78c81040e2391b441c14838624a0d
crod1234 <crod1234@gmail.com>
2021-10-22 15:19:42 -0400

    added new blog

--- bagpipe-blog/index.php
+++ bagpipe-blog/index.php
@@ -23,6 +23,7 @@
 		<section class="blog-link-section">	
 			<div class="blog-links">
 				<ul>
+					<li class="blog-link"><a class="section-title" href="/bagpipe-blog/playing-your-first-bagpipe-solo-competition.php">Playing Your First Bagpipe Competition</a></li> 
 					<li class="blog-link"><a class="section-title" href="/bagpipe-blog/how-to-memorize-bagpipe-tunes-faster.php">How to Memorize Bagpipe Tunes Faster</a></li> 
 					<li class="blog-link"><a class="section-title" href="/bagpipe-blog/learning-to-play-the-bagpipes-if-you-already-play-an-instrument.php">Learning to Play the Bagpipes if You Already Play an Instrument</a></li> 
 					<li class="blog-link"><a class="section-title" href="/bagpipe-blog/how-to-use-bagpipes-with-other-musical-instruments.php">How do You Use Bagpipes with Other Instruments?</a></li>
