c4c16fe75ecbeb52d754a22fd7a6bab0d05a8e4a
crod1234 <crod1234@gmail.com>
2021-11-11 11:42:15 -0500

    added new blog

---
+++ bagpipe-blog/when-to-quit-your-bagpipe-band.php
@@ -0,0 +1,37 @@
+<?php require "../components/head.php"?>
+		<title>When to Quit Your Bagpipe Band</title>
+		<?php $content = "Thinking about leaving your pipe band?  Here's when you should leave your bagpipe band.  Music bands are loaded with complex personalities and if your band is not going anywhere or, worse, is toxic you should move on.";?>
+		<meta name="description" content="<?php echo $content;?>">
+		<meta property="og:description" content="<?php echo $content;?>">
+	</head>	
+	<body>
+		<?php require "../components/googleTagBody.php";?>
+			<?php require "../components/learn-call.php";?>
+			<?php require "../components/nav.php";?>
+			
+			<section class="blog-post-section">
+				<div class="blog-post-div">
+					<img class="blog-post-image" src="../img/bagpipe-band-breakup.jpeg" alt="Couple Breaking Up">
+					<h1 class="section-title blog-post-title">When to Quit Your Bagpipe Band</h1>
+        
+					<p class="section-p blog-post-p"><span class="bold">They say that breaking up is hard to do…</span></p>
+					<p class="section-p blog-post-p">But it’s worth it if the relationship is not going well.  One of the biggest hurdles to getting better in bagpipes is when you’re in an environment that isn’t fostering your development as a musician.  It is even worse when the environment actually sets you up to fail.</p>
+					<p class="section-p blog-post-p">Bagpipe bands can be a lot like families.  In families there are lots of dynamics.  These dynamics consist of directing what to do with the band and how to lead it forward—or not.  And some families are <span class="bold">@#$%&!</span> up.</p>
+					<p class="section-p blog-post-p">In band there usually is a hierarchy.  These hierarchies consist of a few players.  Perhaps it is a pipe major coupled with a big ego.  Perhaps the pipe major has grown resentful because they see that you want to improve (or that you’re a much better player).  And this desire to improve is seen as a threat to the pipe major’s role in the band.  This may then breed contempt and resentment.  Remember how I said that bands are like families?  Not all bands and families are rose-y.</p>
+					<p class="section-p blog-post-p">As you begin to make suggestions like…. <span class="italic">“Hey, maybe we should practice regularly?”  “Maybe we should learn some new tunes?”  “Maybe we should tune the band?”  “Maybe we should hire an instructor?”  “Maybe we should actually try to get better as a band rather than just get drunk after a parade?”</span>  The pipe major or other members of the hierarchy shoot down your ideas.  They may even get downright hostile, which is a clear sign that you should join another band.</p>
+					<p class="section-p blog-post-p">Perhaps the band hierarchy isn’t so hostile.  Perhaps they’re passive and they’ll say things like…  <span class="italic">“Well, this is more of a party band.”</span>  Or… <span class="italic">“the band is fine.”</span>  They’ll insert an excuse.</p>
+					<p class="section-p blog-post-p">I’ve had a few members of bands approach me about fixing their band.  And I am all about fixing a band—as long as the band wants to be fixed.  But if you are a black sheep in the band (and perhaps you have a buddy who also is one) then your best bet is to find another band that actually cares.  Not only will you have the psychological benefit of removing yourself from a passive or, perhaps, even a toxic, environment, but you will automatically improve by simply being around better, passionate musicians.  It’s better to be the worst player in a band that is good or trying to improve than to be the best player in a band that sucks or—worse—just doesn’t care.</p>
+					<p class="section-p blog-post-p">Fortunately in bagpipe bands we can actually choose who we want to be with.  When you play with the new band you’ll be so much happier.</p>
+
+          <p class="section-p blog-post-p section-border-space"><span class="italic">Published: November 11th, 2021</span></p>
+					
+					<div class="section-border"></div>
+					<p class="section-p questions">Have any questions? Shoot me an <a href="mailto: nycbagpipes@gmail.com">email</a>.</p>
+					
+			</section>
+							
+			<?php require "../components/footer.php";?>
+		</div>
+		<script src="../script.js"></script>
+	</body>
+</html>
