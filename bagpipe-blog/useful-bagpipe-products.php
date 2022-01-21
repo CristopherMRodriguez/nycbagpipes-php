9d07dbf418b0bd4fb39d58b3caab2dc169090bfb
crod1234 <crod1234@gmail.com>
2022-01-21 15:18:23 -0500

    new blog

---
+++ bagpipe-blog/useful-bagpipe-products.php
@@ -0,0 +1,138 @@
+<?php require "../components/head.php"?>
+		<title>Useful Bagpipe Accessories</title>
+		<?php $content = "As you may already know bagpiping can be pricey.  If you are a competitor bagpiping can eat a hole into your wallet between admission costs, gas, tolls, hotels, and food.  Simple products, as soon as they are marketed for bagpiping, have a magical tendency to go up in price.  (After all bagpiping is a small world and bagpipe manufacturers need to make a  living).  That being said, I made a list of products that you may find beneficial to have available.";?>
+		<meta name="description" content="<?php echo $content;?>">
+		<meta property="og:description" content="<?php echo $content;?>">
+	</head>	
+	<body>
+		<?php require "../components/googleTagBody.php";?>
+		<?php require "../components/learn-call.php";?>
+		<?php require "../components/nav.php";?>
+		
+		<section class="blog-post-section">
+			<div class="blog-post-div">
+				<img class="blog-post-image" src="../img/treasure-chest.jpg" alt="A Treasure Chest">
+				<h1 class="section-title blog-post-title">Useful Bagpipe Accessories</h1>
+				<ol class="blog-list">
+					<li class="blog-list-item">Chanter Cap</li>
+					<li class="blog-list-item">Electronic Chanter</li>
+					<li class="blog-list-item">Reed Shaver</li>
+					<li class="blog-list-item">Bagpipe Case</li>
+					<li class="blog-list-item">Reed Case</li>
+					<li class="blog-list-item">Rubber Gloves</li>
+					<li class="blog-list-item">Dental Elastic Bands</li>
+					<li class="blog-list-item">Spare Reeds</li>
+					<li class="blog-list-item">Drone Corks</li>
+					<li class="blog-list-item">Folders and Books</li>
+					<li class="blog-list-item">Metronome</li>
+					<li class="blog-list-item">Hearing Protection</li>
+				</ol>
+
+				<div class="section-border"></div>
+
+				<p class="section-p blog-post-p">As you may already know bagpiping can be pricey.  If you are a competitor bagpiping can eat a hole into your wallet between admission costs, gas, tolls, hotels, and food.  Simple products, as soon as they are marketed for bagpiping, have a magical tendency to go up in price.  (After all bagpiping is a small world and bagpipe manufacturers need to make a  living).  That being said, I made a list of products that you may find beneficial to have available.</p>  
+				<p class="section-p blog-post-p"><span class="italic">Disclaimer:</span> Now, first off, I am a fan of being thrifty.  I also have a background in audio engineering where I’ve seen millions of people on forums go on about the latest expensive gadget they want to buy.  I believe there is a psychological reason for this.  The mind likes to justify buying things because it “will” make the person sound better.  While that may apply in some cases, the number one way to sound better is to practice.  There’s a good chance that a talented musician can make a crappy instrument sound good, but a poor musician can never make the best instrument in the world sound good.  It’s best to put the time in to get better and not necessarily go on a spending spree.</p>  
+				<p class="section-p blog-post-p">So here’s a list of useful accessories for bagpiping.</p>  
+
+        <p class="section-border-space"></p>
+				<div class="section-border"></div>
+
+        <h3 class="section-subtitle blog-h3">Chanter Caps</h3>
+				<p class="section-p blog-post-p">I remember when I got started piping I saw these cool looking things go on a chanter.  They looked smart and super pro, but they add a very practical purpose: especially when they do a great job of preventing mold from growing on your favorite reed.  They can be pretty cheap, around the $10 range.  If you’re pretty handy you could even make one yourself since it’s just a container with a thumb screw.</p>  
+				<p class="section-p blog-post-p">Some are more fancy and provide humidity to the chanters.  I’ve heard mixed things about them, but some people like them.  It’s supposed to allow a reed which may not be played often to maintain the same amount of ease to play since it won’t dry out.  (A little bit of moisture for wood is usually a good thing).  Of course, that’s entirely up to you if you need that extra feature and they are not nearly as cheap as the simpler chanter caps.</p>  
+				<p class="section-p blog-post-p">Having a simple chanter cap will protect reeds from mold and unnecessary damage.  You definitely shouldn’t be leaving your chanters and reeds in your bag after you’re done playing.  So it’s a good investment.  If you happen to have more than one chanter you may consider getting more than one cap.</p>  
+
+        <p class="section-border-space"></p>
+				<div class="section-border"></div>
+
+        <h3 class="section-subtitle blog-h3">Electronic Chanters</h3>
+				<p class="section-p blog-post-p">I’ve written about these before: <a href="https://www.nycbagpipes.com/bagpipe-blog/ross-vs-faegerstrom-vs-blair-electronic-bagpipe-review.php">there are pros</a> and <a href="https://www.nycbagpipes.com/bagpipe-blog/is-an-electronic-bagpipe-good.php">there are cons</a> to electronic chanters.  First off they are not cheap, but on the other hand they provide a lot of opportunity to get extra practice in.</p>  
+
+        <p class="section-border-space"></p>
+				<div class="section-border"></div>
+
+        <h3 class="section-subtitle blog-h3">Reed Shaver</h3>
+				<p class="section-p blog-post-p">I’ve been told about these from a friend.  Although I don’t personally own one yet, <a href="https://www.reedgeek.com/product/reedgeek-doublegeek/">I am very close to “biting the bullet” and buying one.</a>  It’s a very sharp knife that will allow you to manipulate your bagpipe reed.  And although you could manipulate reeds using a regular razor blade, being that many reeds start in the $15 range and up you could risk damaging the reed which isn’t cheap.  If you’re doing a lot of reed work then this may be a good investment, especially if you have several chanters that you use.  For bagpiping purposes go with the “bullet” version which allows for fine precision shaving.</p>  
+		 
+
+        <p class="section-border-space"></p>
+				<div class="section-border"></div>
+
+        <h3 class="section-subtitle blog-h3">Bagpipe Case</h3>
+				<p class="section-p blog-post-p">I started with one of these old ones that came with my original set of bagpipes in high school.  Although classic these types are not very secure.  A good bang and the pipes would fall out.  Bagpipes need a good case during travel.  They also protect against sudden temperature changes, which can prevent wood from cracking.  If you have imitation ivory project mounts or ferrules, these can easily break.  Therefore a good case will help.</p>  
+				<p class="section-p blog-post-p">Being that I like to have all of my supplies with me for emergencies I wanted a case that would hold everything I would need.  <a href="https://www.fusion-bags.com/products/urban-bagpipe-gig-bag">I went with the Fusion bag.</a>  It’s very sturdy and the material and stitching are very high quality.  The case can take a lot of abuse and travel so the quality withstands the wear and tear.  It is a little bulky, however that allows me to hold all the stuff I want for those emergency situations.  Also there’s an accessories holder, which is easily detachable from the inside so you can reserve that for the things that you don’t necessarily need to bring with you everywhere.</p>  
+				<p class="section-p blog-post-p">Other companies have <a href="https://bagpipeskin.com">cases that are much more slim.</a>  Some are also <a href="http://bagpipecase.com/product/bagpiper-backpack/">super compact for carry on airline travel,</a> perfect if you don’t want to risk damaging your prized possession.</p>  
+				<p class="section-p blog-post-p">A good case is always a good investment.  However, if you want to be super thrifty, like someone I know in my band, you could always use a tool box.</p>  
+        <img class="blog-post-image" src="../img/tool-box.jpg" alt="A Large Tool Box">
+        <p class="section-p blog-post-p"><span class="italic">It's a bit ugly though...</span></p>  
+
+        <p class="section-border-space"></p>
+				<div class="section-border"></div>
+
+        <h3 class="section-subtitle blog-h3">Reed Case</h3>
+				<p class="section-p blog-post-p">These are useful but probably the biggest rip off (and the easiest to find a cheap substitute).</p>  
+				<p class="section-p blog-post-p">You could opt for an old camera film canister like I used to when I was young…</p>  
+				<p class="section-p blog-post-p">Shepherd makes one but I don’t like it since it’s flimsy: the latches broke and it doesn’t hold the reeds in the mould well.  At $17 (for cheap plastic) and considering it doesn’t do a good job, it’s overpriced.  However a case is important to protect each of your reeds.  Sometimes I like to hold onto the cardboard boxes that the reeds come in when I order them, they make great cases.  Other companies have humidifiers built into them but that also greatly adds to the price.  (As I said before, add bagpipe to it and the price goes up a lot).  However there are many things that can be used to safely hold your reeds which you may already have at home.</p>  
+        <img class="blog-post-image" src="../img/film-canister.jpg" alt="A film canister">
+        <p class="section-p blog-post-p"><span class="italic">A simple film canister can hold your bagpipe reeds safely…</span></p>  
+
+        <p class="section-border-space"></p>
+				<div class="section-border"></div>
+
+        <h3 class="section-subtitle blog-h3">Rubber Gloves</h3>
+				<p class="section-p blog-post-p">This was a cool trick that someone told me.  Use these to help you get a better grip if a chanter gets stuck in the stock.  One reason why it can be difficult to get a chanter off is because our hands are sweaty.  Use caution though to never exert great force or motion on any part of your bagpipes as it can damage the pipes.  <span class="bold">And never grab and pull out a bagpipe chanter by the bottom (sole area).</span>  If it is a wooden chanter it WILL crack, and that would be an expensive loss.  Resist the temptation at all costs!  Grab it by the top, aka the bulb, with the gloves on to give you some extra traction.</p>   
+
+        <p class="section-border-space"></p>
+				<div class="section-border"></div>
+
+        <h3 class="section-subtitle blog-h3">Dental Elastic Bands</h3>
+        <p class="section-p blog-post-p">These are also a great way to lighten up a tough reed.  <a href="https://www.amazon.com/gp/product/B00BWPXP3Q/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1">You can get these for cheap online.</a>  Get 3/8” medium size.  Wrap it around twice on the shoulder of the reeds, below the blades.</p>  
+
+
+        <p class="section-border-space"></p>
+				<div class="section-border"></div>
+
+        <h3 class="section-subtitle blog-h3">Spare Reeds</h3>
+				<p class="section-p blog-post-p">Always have backup reeds.  Reeds break.  And you don’t want to be at a funeral gig or competition to find that out at the last minute.  This includes drone reeds.  At least keep an old set of drones reeds just in case (no pun intended).  It’s also good to have a backup practice chanter reed.  By having an extra one you won’t have to spend on shipping if it breaks.  And...</p>  
+
+        <p class="section-border-space"></p>
+				<div class="section-border"></div>
+
+        <h3 class="section-subtitle blog-h3">Drone Corks</h3>
+				<p class="section-p blog-post-p">It’s also good to keep small rubber done corks too in case you need to seal a drone that may be acting up.  In an emergency this can bail you out.</p>   
+
+        <p class="section-border-space"></p>
+				<div class="section-border"></div>
+
+        <h3 class="section-subtitle blog-h3">Folders and Books</h3>
+				<p class="section-p blog-post-p">I am a huge fan of organizing my music.  <a target="_blank" href="https://www.amazon.com/gp/product/B01J04Y4A8/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B01J04Y4A8&linkCode=as2&tag=nycbagpipes-20&linkId=3c81db66e070f0fc941a3fc3b7f50276">I have one book,</a> which I have dedicated to interesting tunes.</p>  
+				<p class="section-p blog-post-p"><a target="_blank" href="https://www.amazon.com/gp/product/B099WHJSTP/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B099WHJSTP&linkCode=as2&tag=nycbagpipes-20&linkId=6c1fabffefaf92b278a11be31ad52c35">I also have a folder</a> (which I absolutely love) with several pockets that I can use to split up my music: new solo material, old solo material, competition band music, band peripheral music, exercises, etc.  Within seconds I find the tune that I need to reference, rather than going through a pile.  And best of all: if I’m going anywhere it sleekly fits into a backpack with my practice or electronic chanter so I can get extra practice or learn new tunes on the road.  A sheet book or folder can be under $10 each online and they are a fantastic investment.</p>  
+				<p class="section-p blog-post-p"></p>  
+
+        <p class="section-border-space"></p>
+				<div class="section-border"></div>
+
+        <h3 class="section-subtitle blog-h3">Metronome</h3>
+				<p class="section-p blog-post-p">Everyone should be using a metronome to practice.  Although I usually advise getting away from electronic devices while practicing since they CAN and WILL distract, I have found smartphone metronome apps to be better than hardware metronomes.  They are cheap (or often free) and highly programmable to do any kind of rhythm you need.  <a href="http://eumlab.com/pro-metronome/">I am a big fan of this metronome</a>, which is freemium (I paid only $2.99 for the upgraded features).</p>  
+				<p class="section-p blog-post-p">I have not had great experience with hardware electronic metronomes; ALMOST ALL of them have broken on me.  The last one I got, which was a <a href="https://www.tama.com/eu/products/detail/rw200.html">Tama for around $70</a>  immediately started to have issues (barely audible volume output). Good luck connecting to a customer service rep.</p>  
+				<p class="section-p blog-post-p">Phone metronomes are great too cause you can use a wireless headphone with the metronome.  You can then conveniently use that while playing on the highland bagpipes with a direct connection of your ear to the metronome click to get closer precision while you practice.</p>  
+				<p class="section-p blog-post-p">On another occasion I did happen to buy a special bagpipe metronome phone app.  This app allows for programming pulsed beats for strathspey, reel, and 2/4 march playing, amongst other features.  However I have a lot of mixed feelings about it, more so on the negative side.  Considering that, it is extremely overpriced for a phone app: $20 (again bagpipe products costing more).  I wonder if it may actually do more harm than good. I spoke with a good snare drummer about it and he said that he was generally suspicious of any electronic device that did pulsing since it robs you of the human touch.</p>  
+
+        <p class="section-border-space"></p>
+				<div class="section-border"></div>
+
+        <h3 class="section-subtitle blog-h3">Hearing Protection</h3>
+				<p class="section-p blog-post-p">Last but not least.  Buy hearing protection, whether it be cheap, foam ones (under $5) or the silicon ones ($10).  <a href="https://rghardiestore.com/pipe-band-ear-protection">I also like the R.G. Hardie ones.</a> (I think they sound a little better than my generic $10 silicon ones, but they cost more in the $20 range.  Therefore it's up to you to decide if it's worth it and the difference may be negligible).  If you lose your hearing your piping will end.  I know people who have hearing damage in their left ear (because that’s the side the pipes are on).  If you play in band you should always have hearing protection on, ESPECIALLY when playing indoors and with snare drummers.  I find also that the silicon hearing protectors also allow you to “focus in” on your immediate environment and tune out the background noises.  Although it may take some getting use to, you will get used to it.  Don’t lose your hearing!</p>  
+				<p class="section-p blog-post-p">You can also get ones that are molded by a professional and these I hear (again no pun intended) are even better.  They can run you somewhere in the $100 range but they will provide you with some added benefits.</p>  
+	
+        <p class="section-border-space"></p>
+				<div class="section-border"></div>
+				<p class="section-p questions">Happy Piping.  Have any questions? Shoot me an <a href="mailto: nycbagpipes@gmail.com">email</a>.</p>
+				
+			</div>
+		</section>
+	
+		<?php require "../components/footer.php";?>
+		<script src="../script.js"></script>
+	</body>
+</html>
